9a6945c52ce7f65a6f1c5be72cc0373be006c3c2
magicdawn <magicdawn@qq.com>
2017-05-23 23:04:54 +0800

    test: add tiz basic test

---
+++ packages/tiz/test/util.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const LocalUtil = require('../lib/util.js')
+
+describe('LocalUtil', function() {
+  it('.basenameNoExt', function() {
+    LocalUtil.basenameNoExt(__filename).should.equal('util')
+  })
+})

5e2b0abe283cc6a6858084a18eabe6f42693c910
magicdawn <magicdawn@qq.com>
2017-05-31 00:50:32 +0800

    core: views load template engine from project, not framework

--- packages/tiz/lib/mixin/setupViews.js
+++ packages/tiz/lib/mixin/setupViews.js
@@ -1,8 +1,11 @@
 'use strict'
 
+const dirname = require('path').dirname
 const debug = require('debug')('tiz:setupViews')
 const consolidate = require('consolidate')
 const views = require('koa-generic-views')
+const resolveFrom = require('resolve-from')
+const importFrom = require('import-from')
 
 module.exports = function() {
   const config = this.config.views
@@ -16,9 +19,24 @@ module.exports = function() {
     defaultExt: config.extension,
   })
 
+  const resolveFromProjectHome = resolveFrom.bind(null, this.projectHome)
+  const importFromProjectHome = importFrom.bind(null, this.projectHome)
+  const consolidateDir = dirname(require.resolve('consolidate'))
+
   for (let name in config.map) {
     const ext = name
     const engineName = config.map[name]
+
+    const has = resolveFrom.silent(consolidateDir, engineName)
+    if (!has) {
+      // if `consolidate` can not require('engine')
+      // we manualy set `consolidate.requires` = `require from projectHome (engineName)`
+      // see node core flag, symlink
+      const engineModule = importFromProjectHome(engineName)
+      consolidate.requires[engineName] = engineModule
+    }
+
+    // add to `koa-generic-views`
     this.engine(ext, consolidate[engineName])
   }
 
